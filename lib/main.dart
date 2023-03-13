356dd5dc2ef9f1679053599524d31a32c03f69d6
Alain Patrick dela Pena <alaindee@gmail.com>
2023-03-13 16:57:29 +1100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,303 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //int _counter = 0;
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Appontment',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  String formatDate() {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat.yMMMMEEEEd().format(now);
+    return formattedDate;
+  }
+
+  String greetings() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Morning';
+    }
+    if (hour < 17) {
+      return 'Afternoon';
+    }
+    return 'Evening';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50, left: 30, right: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.brown.shade300,
+                    radius: 30.0,
+                    child: const Text('AP'),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: Text(
+                      'Patrick dela Pena',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w400),
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Good ${(greetings())}!',
+                      style: const TextStyle(
+                          fontSize: 28.0, fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 1.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Today is ${(formatDate())}',
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Row(
+                  children: const [
+                    Text(
+                      'Your next appointment:',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 10.0),
+                      padding: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(7)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(.5),
+                                spreadRadius: 2,
+                                blurRadius: 4,
+                                offset: const Offset(0, 2))
+                          ]),
+                      child: Column(children: [
+                        Row(
+                          children: const [
+                            Text(
+                              'Appointment Type',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Spacer(flex: 1),
+                            Icon(Icons.chevron_right)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: Container(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: const Text(
+                                    'appointment description, Fusce congue fringilla eros, eget malesuada felis tincidunt non. Duis non consequat nisl, non hendrerit enim. Nunc malesuada ac felis sed aliquam.',
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(top: 5.0),
+                              padding: const EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade100,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(10.0))),
+                              child: const Text(
+                                'Mon, Mar 13 3:30 pm',
+                                style: TextStyle(fontWeight: FontWeight.w300),
+                              ),
+                            ),
+                            const Spacer(
+                              flex: 1,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 5.0),
+                              padding: const EdgeInsets.all(10.0),
+                              child: const Text(
+                                'Mr Peter Pan',
+                                style: TextStyle(fontWeight: FontWeight.w300),
+                              ),
+                            )
+                          ],
+                        )
+                      ]),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              Center(
+                child: _widgetOptions.elementAt(_selectedIndex),
+              ),
+              // const Text(
+              //   'You have pushed the button this many times:',
+              // ),
+              // Text(
+              //   '$_counter',
+              //   style: Theme.of(context).textTheme.headlineMedium,
+              // ),
+            ],
+          ),
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            //label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined),
+            //label: 'Appointments',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.note),
+          //   label: 'Notes',
+          // ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            //label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
